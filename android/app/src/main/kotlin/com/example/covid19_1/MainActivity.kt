204c3927e4f6c4069a0632801d6500c3c516ae0b
ridh7 <ridhwik712000@gmail.com>
2020-07-13 19:17:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/covid19_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid19_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/info_screen.dart
@@ -0,0 +1,197 @@
+import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class InfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            MyHeader(
+              image: 'assets/icons/coronadr.svg',
+              textTop: 'Get to know',
+              textBottom: 'about COVID-19.',
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Symptoms',
+                    style: kTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SymptomCard(
+                        image: 'assets/images/headache.png',
+                        title: 'Headache',
+                        isActive: true,
+                      ),
+                      SymptomCard(
+                        image: 'assets/images/caugh.png',
+                        title: 'Cough',
+                      ),
+                      SymptomCard(
+                        image: 'assets/images/fever.png',
+                        title: 'Fever',
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Prevention',
+                    style: kTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    image: 'assets/images/wear_mask.png',
+                    title: 'Wear face mask',
+                    desc:
+                        'Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks.',
+                  ),
+                  PreventCard(
+                    image: 'assets/images/wash_hands.png',
+                    title: 'Wash your hands',
+                    desc:
+                        'Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks.',
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String desc;
+
+  const PreventCard({
+    Key key,
+    this.desc,
+    this.image,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 136,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    blurRadius: 24,
+                    color: kShadowColor,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(image),
+            Positioned(
+              left: 130,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 15,
+                ),
+                height: 136,
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      '$title',
+                      style: kTitleTextstyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      '$desc',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset('assets/icons/forward.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SymptomCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+
+  const SymptomCard({
+    Key key,
+    this.image,
+    this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          isActive
+              ? BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 30,
+                  color: kActiveShadowColor,
+                )
+              : BoxShadow(
+                  offset: Offset(0, 3),
+                  blurRadius: 6,
+                  color: kShadowColor,
+                ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 70),
+          Text(
+            '$title',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
